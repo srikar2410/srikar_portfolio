d89dff1a6a0f14b69186518d28254a14d82708c1
srikar2410 <yakkalurisrikar@gmail.com>
2024-12-23 13:19:43 +0530

    Add files via upload

---
+++ portfolio1.js
@@ -0,0 +1,28 @@
+// let counter = 0;
+// setInterval(() => {
+
+//     const el = document.querySelector('.circle');
+//     const elValue = Number(el.getAttribute('data-percentage'));
+//     if(counter !== elValue) {
+//     counter++;
+//     el.innerHTML = `${counter}%`;
+//     }
+// }, 80);
+// let cirle = document.querySelector(".circle"),
+// circle-text = document.querySelector(".circle-text");
+
+// let progressStartValue = 0,
+// progressEndValue = 95,
+// speed = 100;
+
+// let progress = setInterval(() =>{
+//     progressStartValue++;
+
+//     progressValue.textcontent = `${progressStartValue}`
+
+//     if(progressStartValue == progressEndValue){
+//          clearInterval(progress);
+
+//     }
+// }, speed);
+
